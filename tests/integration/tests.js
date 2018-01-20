90aba5bca7a1fc9925c503930643afecfded1494
Marta <martadabrowka@gmail.com>
2018-01-20 16:04:59 +0000

    passed chain opeartion of subtraction and division returning a decimal number

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -141,4 +141,15 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('-1')
   })
 
+  it('should update the display of the running total with the result of chain operation of subtraction and division which is a decimal', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number9')).click();
+    element(by.css('#operator_subtract')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('3.5')
+  })
+
 });
