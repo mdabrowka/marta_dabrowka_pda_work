9950c682baee6d4e20b6c73a5f1aff0c8fc98bca
Marta <martadabrowka@gmail.com>
2018-01-20 15:48:08 +0000

    passed chain opeartion sum, subtraction, multiplication and division

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -111,4 +111,22 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('14')
   })
 
+  it('should update the display of the running total with the result of chain operation of sum, subtraction, multiplication and division', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number1')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#operator_add')).click();
+    element(by.css('#number7')).click();
+    element(by.css('#number7')).click();
+    element(by.css('#operator_subtract')).click();
+    element(by.css('#number9')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('8')
+  })
+
 });
