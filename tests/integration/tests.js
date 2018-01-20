368fdc0ba91321badc5708b815a0c89c0c6508e2
Marta <martadabrowka@gmail.com>
2018-01-20 15:37:38 +0000

    passed chain operation division and sum

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -65,4 +65,16 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('9')
   })
 
+  it('should update the display of the running total with the result of chain operation of division and sum', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number2')).click();
+    element(by.css('#number7')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#number3')).click();
+    element(by.css('#operator_add')).click();
+    element(by.css('#number5')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('14')
+  })
+
 });
