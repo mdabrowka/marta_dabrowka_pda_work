32b098f722e9a12f4bd5f0d833a8442207f774a3
Marta <martadabrowka@gmail.com>
2018-01-20 15:42:42 +0000

    passed chain operation multiplication and division

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -99,4 +99,16 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('16')
   })
 
+  it('should update the display of the running total with the result of chain operation of multiplication and division', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number1')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#number7')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#number6')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('14')
+  })
+
 });
