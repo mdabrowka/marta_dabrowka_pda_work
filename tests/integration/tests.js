825f49bbfab85582a630179be18ca4280ea6a93c
Marta <martadabrowka@gmail.com>
2018-01-20 15:40:41 +0000

    passed chain operation subtraction and multiplication

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -88,4 +88,15 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('6')
   })
 
+  it('should update the display of the running total with the result of chain operation of subtraction and multiplication', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number9')).click();
+    element(by.css('#operator_subtract')).click();
+    element(by.css('#number7')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#number8')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('16')
+  })
+
 });
